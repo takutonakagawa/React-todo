2bfe85cade68d7f3516c5ccab16ec7dc63055deb
tackt21 <82143268+takutonakagawa@users.noreply.github.com>
2021-09-06 16:52:09 +0900

    css css コピー

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,40 @@
 import "./styles.css";
 
 export const App = () => {
-  return <div></div>;
+  return (
+    <>
+      <div>
+        <input />
+        <button>追加</button>
+      </div>
+      <div>
+        <p>未完了のTODO</p>
+        <ul>
+          <div>
+            <li>aiueo</li>
+            <button>完了</button>
+            <button>削除</button>
+          </div>
+          <div>
+            <li>aiueo</li>
+            <button>完了</button>
+            <button>削除</button>
+          </div>
+        </ul>
+      </div>
+      <div>
+        <p>未完了のTODO</p>
+        <ul>
+          <div>
+            <li>aiueo</li>
+            <button>戻す</button>
+          </div>
+          <div>
+            <li>aiueo</li>
+            <button>戻す</button>
+          </div>
+        </ul>
+      </div>
+    </>
+  );
 };
